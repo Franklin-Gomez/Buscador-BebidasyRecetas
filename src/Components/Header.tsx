f5dc3c831b9852539d78a619e96536ef27bc5182
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-09 11:59:08 -0500

    identify the principal page

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,29 +1,35 @@
-import { Link , NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 
 export default function Header() {
-  return (
-    <header className="bg-slate-800">
-        <div className="mx-auto container px-5 py-16">
-            <div className="flex justify-between items-center">
 
-                <div>
-                    <img src="./logo.svg" alt="logotipo" className="w-32" />
+    const loading = useLocation();
+
+    console.log( loading )
+
+    return (
+        <header className="bg-slate-800">
+            <div className="mx-auto container px-5 py-16">
+                <div className="flex justify-between items-center">
+
+                    <div>
+                        <img src="./logo.svg" alt="logotipo" className="w-32" />
+                    </div>
+
+                    <nav className="flex gap-4">
+                        <NavLink 
+                            to='/'
+                            className={({isActive }) => isActive ?  "text-orange-500 uppercase font-bold" : "text-white uppercase font-bold"  }
+                        > Inicio </NavLink>
+
+                        <NavLink
+                            to='/favoritos'
+                            className={({isActive }) => isActive ?  "text-orange-500 uppercase font-bold" : "text-white uppercase font-bold"  }
+                        > Favoritos </NavLink>
+                    </nav>
+
                 </div>
-
-                <nav className="flex gap-4">
-                    <Link 
-                        to='/'
-                        className="text-white uppercase font-bold"
-                    > Inicio </Link>
-
-                    <Link
-                        to='/favoritos'
-                        className="text-white uppercase font-bold"
-                    > Favoritos </Link>
-                </nav>
-
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
