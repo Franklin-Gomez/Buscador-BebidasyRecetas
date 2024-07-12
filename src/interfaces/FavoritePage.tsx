0881622189a9f78910d3c46f4f64dfc86eda5e22
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-12 09:01:01 -0500

    working on favoritePage

--- src/interfaces/FavoritePage.tsx
+++ src/interfaces/FavoritePage.tsx
@@ -1,8 +1,38 @@
+import { useMemo } from "react"
+import DrinkCard from "../Components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
+
 
 export default function FavoritePage() {
+
+  const favorites = useAppStore((state) => state.favorites)
+
+  const hasFavorites = useMemo(() => favorites.length > 0 , [ favorites]) 
+
   return (
+
     <>
-        <h1> Desde favorite </h1>
+
+      <h1 className="text-6xl font-extrabold"> Favoritos </h1>
+
+      {  hasFavorites ? 
+      
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          { favorites.map ((drink) => ( 
+            <DrinkCard 
+              key={drink.idDrink}
+              drink={drink}
+            />
+          ))}
+        </div>
+      
+        :  
+      
+        <h1> Agrega Recetas  </h1>
+      
+      }
+
     </>
+
   )
 }
