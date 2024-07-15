6fedf40bd8f38e587ac097037c7d47a5ba259ca6
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-15 12:01:20 -0500

    add LinkNavigate

---
+++ src/Components/Header.tsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom"
+
+export default function Header() {
+    return (
+        <div>
+            <div>
+
+            </div>
+
+            <div>
+
+                <NavLink to="/"> INICIO </NavLink>
+                <NavLink to="/favoritos"> FAVORITOS </NavLink>
+            
+            </div>
+        </div>
+    )
+}
